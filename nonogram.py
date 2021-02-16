16e2852bbe8a0299c337a37bd381ff6dfde4b554
Urgith <patrykolejniczak7@gmail.com>
2021-02-16 16:42:44 +0100

    pierwsze poprawki

--- nonogram.py
+++ nonogram.py
@@ -1,14 +1,12 @@
 import pygame
+import sys
 
 
-def additional(analyzed_list):
+def additional(a_list):
   maximum = 0
 
-  for element in analyzed_list:
-    length = len(element)
-
-    if length > maximum:
-      maximum = length
+  for element in a_list:
+    maximum = max(len(element), maximum)
 
   return maximum
 
@@ -18,50 +16,46 @@ class Nonogram:
     pygame.display.set_caption('Nonogram')
     pygame.init()
 
-    self.x_additional = additional(x)
-    self.y_additional = additional(y)
     self.x = x
     self.y = y
+    self.len_x = len(x)
+    self.len_y = len(y)
+    self.x_additional = additional(y)
+    self.y_additional = additional(x)
 
-    self.okno_gry = pygame.display.set_mode((20*(len(x) + self.x_additional), 20*(len(y) + self.y_additional)))
-    self.font = pygame.font.SysFont(None, 25)
-
-    self.loop = True
-    while self.loop:
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          self.loop = False
-
-      self.draw()
+    self.screen = pygame.display.set_mode((20*(self.len_x + self.x_additional), 20*(self.len_y + self.y_additional)))
+    self.font = pygame.font.SysFont(None, 20)
 
   def draw(self):
-    self.okno_gry.fill((255, 255, 255))
+    self.screen.fill((255, 255, 255))
 
-    for i in range(len(self.x) + self.x_additional):
-      if i < self.x_additional - 1:
-          pygame.draw.line(self.okno_gry, (0, 0, 0), (20*(i + 1), 20*self.x_additional), (20*(i + 1), 20*(len(self.y) + self.y_additional)))
-          continue
-
-      pygame.draw.line(self.okno_gry, (0, 0, 0), (20*(i + 1), 0), (20*(i + 1), 20*(len(self.y) + self.y_additional)))
-
-    for i in range(len(self.y) + self.y_additional):
-      if i < self.y_additional - 1:
-          pygame.draw.line(self.okno_gry, (0, 0, 0), (20*self.y_additional, 20*(i + 1)), (20*(len(self.x) + self.x_additional), 20*(i + 1)))
-          continue
-
-      pygame.draw.line(self.okno_gry, (0, 0, 0), (0, 20*(i + 1)), (20*(len(self.x) + self.x_additional), 20*(i + 1)))
-
+    self.create_grid()
     self.inserting_numbers()
     pygame.display.update()
 
   def inserting_numbers(self):
     for i in range(len(self.x)):
       for j in range(len(self.x[i])):
-        self.okno_gry.blit(self.font.render(str(self.x[i][j]), True, (0, 0, 0)), (20*(self.x_additional + i) + 5, 20*j + 5))
+        self.screen.blit(self.font.render(str(self.x[i][j]), True, (0, 0, 0)), (20*(self.x_additional + i) + 7, 20*j + 4))
 
     for i in range(len(self.y)):
       for j in range(len(self.y[i])):
-        self.okno_gry.blit(self.font.render(str(self.y[i][j]), True, (0, 0, 0)), (20*j + 5, 20*(self.y_additional + i) + 5))
+        self.screen.blit(self.font.render(str(self.y[i][j]), True, (0, 0, 0)), (20*j + 7, 20*(self.y_additional + i) + 4))
+
+  def create_grid(self):
+    for i in range(1, self.len_x + self.x_additional):
+      pygame.draw.line(self.screen, (0, 0, 0), (20*i, 0), (20*i, 20*(self.len_y + self.y_additional)))
+
+    for i in range(1, self.len_y + self.y_additional):
+      pygame.draw.line(self.screen, (0, 0, 0), (0, 20*i), (20*(self.len_x + self.x_additional), 20*i))
+
+    pygame.draw.rect(self.screen, (255, 255, 255), pygame.Rect(0, 0, 20*self.x_additional, 20*self.y_additional))
 
 
-Nonogram([list(range(4)) for i in range(5)], [list(range(4)) for i in range(5)])
+game = Nonogram([list(range(3)) for i in range(5)], [list(range(4)) for i in range(5)])
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      sys.exit()
+
+  game.draw()
